79fc93f9e0d2ffce0d8e8e75611118af6474b193
borednuna <hanunshaka02@gmail.com>
2023-05-10 23:54:02 +0700

    Revert "feat: add db connection test"
    This reverts commit 8169d2fdb5081831d292547e55aacdcd655d3b36.

--- config/globals.js
+++ config/globals.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
-const dbname = process.env.DB_NAME;
-const dbuser = process.env.DB_USERNAME;
-const dbpass = process.env.DB_PASSWORD;
-const dbhost = process.env.DB_HOST;
-const dbport = process.env.DB_PORT;
+const dbname = process.env.DB_NAME || 'test';
+const dbuser = process.env.DB_USERNAME || 'root';
+const dbpass = process.env.DB_PASSWORD || '';
+const dbhost = process.env.DB_HOST || 'localhost';
+const dbport = process.env.DB_PORT || 3306;
 const email = process.env.EMAIL || 'email';
 const emailpass = process.env.PASSWORD || 'password';
 
